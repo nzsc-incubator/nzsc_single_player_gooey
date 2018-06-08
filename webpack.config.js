cf0ff51ef09520b1cfc68c649b915f2aae9d0923
Kyle Lin <kylejlin.us@gmail.com>
2018-06-08 14:50:31 -0700

    Switch mode to production

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ module.exports = {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'build')
   },
-  mode: "development", // Switch to "production" when ready
+  mode: "production",
   module: {
     rules: [
       {
