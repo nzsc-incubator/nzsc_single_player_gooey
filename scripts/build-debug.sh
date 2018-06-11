a6cbd670b536476c0a9ead96d234dc670e10dfbe
Kyle Lin <kylejlin.us@gmail.com>
2018-06-11 15:26:10 -0700

    Copy build/dev setup from nzsc_web_cli

--- scripts/build-debug.sh
+++
@@ -1,4 +0,0 @@
-cargo +nightly build --target wasm32-unknown-unknown \
-&& wasm-bindgen target/wasm32-unknown-unknown/debug/nzsc_single_player_web.wasm --out-dir ./web-src \
-&& npx webpack \
-&& echo "Warning: build-debug deprecated. Use build-release"
---
+++ webpack.dev.config.js
@@ -0,0 +1,10 @@
+const defaultConfig = require('./webpack.config');
+
+module.exports = {
+  ...defaultConfig,
+  mode: "development",
+  watch: true,
+  watchOptions: {
+    ignored: /node_modules/
+  }
+}
